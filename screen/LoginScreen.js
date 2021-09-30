fd9b9b9ea6ca43ad1bcb04f057b668772174cbfc
Aurélien Feignon <aurelien.feignon@gmail.com>
2021-09-30 18:52:56 +0200

    refactor styles

--- screen/LoginScreen.js
+++ screen/LoginScreen.js
@@ -3,7 +3,6 @@ import {
     View,
     Image,
     Dimensions,
-    StyleSheet,
     TouchableHighlight,
     Text,
     SafeAreaView, StatusBar, Keyboard
@@ -17,8 +16,6 @@ import * as SecureStore from 'expo-secure-store';
 import LoginAction from "../store/actions/LoginAction";
 import {InputOutline} from "react-native-input-outline";
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Color from "../utile/Color";
-import Svg, {Path} from "react-native-svg";
 import Animated, {
     useSharedValue,
     useAnimatedStyle,
@@ -36,13 +33,28 @@ const LoginScreen=(props)=>{
     const {width, height}= Dimensions.get('window');
     const inputMail= useRef(null);
     const [errorMail, setErrorMail]=useState(undefined);
-
+    const styles= Styles(height,width);
     const regexEmail=/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     const iconMail= <Icon name={'person'} size={20} color={"#000000"}/>;
     const iconMdp= <Icon name={'lock'} size={20} color={"#000000"}/>;
     const axios= Axios.create({
         baseURL:'https://sheltered-crag-17970.herokuapp.com/api/',
 
+    });
+    const positionLogo= useSharedValue(0);
+    const positionForm= useSharedValue(0)
+    const sizeLogo= useSharedValue(1);
+
+    const animatedLogo= useAnimatedStyle(()=>{
+        return{
+            transform:[{translateY: positionLogo.value,
+            },{scale: sizeLogo.value}],
+        };
+    });
+    const animatedForm= useAnimatedStyle(()=>{
+        return{
+            transform:[{translateY: positionForm.value}]
+        }
     });
 
     useEffect(()=>{
@@ -61,22 +73,6 @@ const LoginScreen=(props)=>{
             closeKeyboard.remove();
         }
     },[]);
-
-    const positionLogo= useSharedValue(0);
-    const positionForm= useSharedValue(0)
-    const sizeLogo= useSharedValue(1);
-
-    const animatedLogo= useAnimatedStyle(()=>{
-        return{
-            transform:[{translateY: positionLogo.value,
-                },{scale: sizeLogo.value}],
-        };
-    });
-    const animatedForm= useAnimatedStyle(()=>{
-       return{
-           transform:[{translateY: positionForm.value}]
-       }
-    });
 
     useEffect(()=>{
         if(!validEmail && mdp === ''){
@@ -120,8 +116,6 @@ const LoginScreen=(props)=>{
             });
     }
 
-
-
     const onClickButtonSignUp=()=>{
 
     }
@@ -129,79 +123,6 @@ const LoginScreen=(props)=>{
     const onPressForgot=()=> {
         console.log('press')
     }
-
-    const styles=StyleSheet.create({
-        buttonSubmit:{
-            width:'100%',
-            marginVertical: 10,
-            paddingVertical:10,
-            borderRadius: 50,
-            borderWidth: 2,
-        },
-        buttonEnable:{
-            backgroundColor: Color.blueGrey,
-            borderColor: Color.nightBlue,
-        },
-        buttonDisable:{
-            backgroundColor:Color.nightBlue,
-            borderColor: Color.yellow,
-        },
-        buttonSubmitText:{
-            textAlign: 'center',
-            fontSize:20,
-            fontWeight:"bold",
-            textTransform:"uppercase"
-        },
-        buttonSubmitTextDisable:{
-            color: Color.yellow,
-        },
-        buttonSubmitTextEnable:{
-            color: Color.yellow
-        },
-        containerForm:{
-            // position:"absolute",
-            bottom:20,
-            display:"flex",
-            justifyContent:"flex-start",
-            height:height*0.5,
-            width:'80%',
-            marginBottom: 5
-        },
-        containerImgBackground:{
-            position:"absolute",
-            height:height*0.6,
-            width:'100%',
-            bottom:0,
-            zIndex:-1,
-        },
-        containerLogo:{
-            width: width*0.7,
-            height:height*0.4,
-            marginTop:height*0.03,
-            marginBottom:height*0.05
-
-        },
-        containerScreen:{
-            flex: 1,
-            backgroundColor: Color.nightBlue,
-            alignItems: 'center',
-        },
-        imgResponsive:{
-            width: "100%",
-            height: "100%",
-            resizeMode:"contain"
-        },
-        input:{
-            marginVertical:10,
-        },
-        textForgotPassword:{
-            color: '#fff',
-        }
-
-    })
-
-
-
 
     return(
 
@@ -252,20 +173,9 @@ const LoginScreen=(props)=>{
                         </TouchableHighlight>
                     </View>
                 </Animated.View>
-                {/*<View>*/}
-                {/*<View style={styles.containerImgBackground}>*/}
-                {/*    <Svg height="100%" width="100%" preserveAspectRatio="none  xMinYMin xMaxYMax meet"  viewBox="100 0 1000 1252.18">*/}
-                {/*        <Path fill={Color.blueGrey}*/}
-                {/*              d="M1125,383.64v868.54H0V43.76C290.36-30.58,612.32-32.4,807,217.05,895.89,330.94,1006.21,378.9,1125,383.64Z"/>*/}
-                {/*    </Svg>*/}
-                {/*</View>*/}
         </SafeAreaView>
     )
-
-
 }
-
-
 
 const mapStateToProps = state => ({
     user: state.user,
--- utile/Styles.js
+++ utile/Styles.js
@@ -1,25 +1,94 @@
-export default {
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    validInput:{
-        height: 40,
-        width:300,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-        backgroundColor:"#fff"
-    },
-    invalidInput:{
-        height: 40,
-        width:300,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-        backgroundColor: "#DC143C"
-    },
+import Color from "./Color";
 
+export default function getStyle(height, width){
+    return{
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        validInput:{
+            height: 40,
+            width:300,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            backgroundColor:"#fff"
+        },
+        invalidInput:{
+            height: 40,
+            width:300,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+            backgroundColor: "#DC143C"
+        },
+        buttonSubmit:{
+            width:'100%',
+            marginVertical: 10,
+            paddingVertical:10,
+            borderRadius: 50,
+            borderWidth: 2,
+        },
+        buttonEnable:{
+            backgroundColor: Color.blueGrey,
+            borderColor: Color.nightBlue,
+        },
+        buttonDisable:{
+            backgroundColor:Color.nightBlue,
+            borderColor: Color.yellow,
+        },
+        buttonSubmitText:{
+            textAlign: 'center',
+            fontSize:20,
+            fontWeight:"bold",
+            textTransform:"uppercase"
+        },
+        buttonSubmitTextDisable:{
+            color: Color.yellow,
+        },
+        buttonSubmitTextEnable:{
+            color: Color.yellow
+        },
+        containerForm:{
+            // position:"absolute",
+            bottom:20,
+            display:"flex",
+            justifyContent:"flex-start",
+            height:height*0.5,
+            width:'80%',
+            marginBottom: 5
+        },
+        containerImgBackground:{
+            position:"absolute",
+            height:height*0.6,
+            width:'100%',
+            bottom:0,
+            zIndex:-1,
+        },
+        containerLogo:{
+            width: width*0.7,
+            height:height*0.4,
+            marginTop:height*0.03,
+            marginBottom:height*0.05
+
+        },
+        containerScreen:{
+            flex: 1,
+            backgroundColor: Color.nightBlue,
+            alignItems: 'center',
+        },
+        imgResponsive:{
+            width: "100%",
+            height: "100%",
+            resizeMode:"contain"
+        },
+        input:{
+            marginVertical:10,
+        },
+        textForgotPassword:{
+            color: '#fff',
+        }
+    }
 }
