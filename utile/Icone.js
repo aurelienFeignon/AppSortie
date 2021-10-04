4e8a62876a9e1360bcbeb9754898b46b6746fbe0
Aurélien Feignon <aurelien.feignon@gmail.com>
2021-10-04 19:37:58 +0200

    icon delete

--- utile/Icone.js
+++ utile/Icone.js
@@ -1,8 +1,11 @@
 import Icon from "react-native-vector-icons/MaterialIcons";
 import React from "react";
 
-export const iconMail= <Icon name={'person'} size={20} color={"#000000"}/>;
+export const iconMail= <Icon name={'email'} size={20} color={"#000000"}/>;
+export const iconCivilite= <Icon name={'person'} size={20} color={"#000000"}/>;
+export const iconUsername= <Icon name={'person-pin'} size={20} color={"#000000"}/>;
 export const iconMdp= <Icon name={'lock'} size={20} color={"#000000"}/>;
 export const iconVisibility= <Icon name={'visibility'} size={20} color={"#000000"}/>;
+export const iconDelete= <Icon name={'delete'} size={20} color={"#000000"}/>;
 
 
