f265d629472fc63419fce4762b24f99d66decffd
Aurélien Feignon <aurelien.feignon@gmail.com>
2021-10-06 23:47:16 +0200

    add icone

--- utile/Icone.js
+++ utile/Icone.js
@@ -7,5 +7,6 @@ export const iconUsername= <Icon name={'person-pin'} size={20} color={"#000000"}
 export const iconMdp= <Icon name={'lock'} size={20} color={"#000000"}/>;
 export const iconVisibility= <Icon name={'visibility'} size={20} color={"#000000"}/>;
 export const iconDelete= <Icon name={'delete'} size={20} color={"#000000"}/>;
+export const iconCode= <Icon name={'code'} size={20} color={"#000000"}/>;
 
 
